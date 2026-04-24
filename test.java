33374ea11962e9e143f58a0804fbcbb4fd487b8f
Max Meyer <72580755+Red-Rax@users.noreply.github.com>
2026-04-23 23:33:05 -0400

    Delete test.java
    file was unneeded

--- test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-
-    public static void main(String[] args) {
-
-    String s1 = new String("Hello");
-    String s2 = new String("Hello");
-    if (s1 == s2) { System.out.println("These two elements have the same reference"); }
-    if (s1.equals(s2)) { System.out.println("These two elements have the same reference"); }
-}
